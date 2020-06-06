bd703a02b61e6115071c6605520977c1decbef39
Dorian Hemerick <dorianhemerick@gmail.com>
2020-06-06 17:17:29 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { Jaden: 'SOHO',
+                  Joji: 'worldstar money',
+                  Drake: 'Passionfruit'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+    return playlist};
+
+function removeFromPlaylist (playlist, artistName){
+    delete playlist.artistName};
